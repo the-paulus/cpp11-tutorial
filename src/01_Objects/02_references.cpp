a9953984c60816d7097f03a62796308e51b2f429
Paul Lyon <pmlyon@gmail.com>
2016-12-31 08:19:51 -0600

    Updated comments for better clarification.

--- src/01_Objects/02_references.cpp
+++ src/01_Objects/02_references.cpp
@@ -10,7 +10,7 @@ int main(int argc, char **argv)
 
     /*
      * References are aliases of another object and do not have an address.
-     * Therefore, they must have an object assigned to them. The type of alias
+     * Therefore, they must be initialized. The type of alias
      * must match the type of the object that it is being initialized with.
      * Below we create an alias' for the object 'ival' by using the '&'
      * modifier.
