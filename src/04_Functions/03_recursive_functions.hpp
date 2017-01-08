5473e33810d4ec2075c20e9f73fde557ee5b9d1d
Paul Lyon <pmlyon@gmail.com>
2017-01-08 12:38:39 -0600

    Fixed typeo.

--- src/04_Functions/03_recursive_functions.hpp
+++ src/04_Functions/03_recursive_functions.hpp
@@ -2,8 +2,9 @@
 #define __RECURSIVE_FUNCTIONS_H__
 
 /*
- * Demonstrations of a recursive function.
+ * Demonstration of a recursive function.
  */
 long long factoral(const int number);
 
 #endif
+
