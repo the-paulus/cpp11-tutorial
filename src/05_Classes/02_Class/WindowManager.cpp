6b49ace7f97292d8decd0b8bc70c73c8beae1e73
Paul Lyon <pmlyon@gmail.com>
2018-02-20 20:41:51 -0600

    Updated WindowManager class.

--- src/05_Classes/02_Class/WindowManager.cpp
+++ src/05_Classes/02_Class/WindowManager.cpp
@@ -2,4 +2,4 @@
 // Created by Paulus on 2/17/18.
 //
 
-#include "WindowManager.h"
+#include "WindowManager.hpp"
--- src/05_Classes/02_Class/WindowManager.hpp
+++ src/05_Classes/02_Class/WindowManager.hpp
@@ -5,9 +5,37 @@
 #ifndef __WINDOWMANAGER_HPP__
 #define __WINDOWMANAGER_HPP__
 
+#include <vector>
+
+#include "Screen.hpp"
+
+using std::vector;
 
 class WindowManager {
+public:
+    typedef struct dimensions_t {
+        unsigned long width = 0;
+        unsigned long height = 0;
+    };
 
+    typedef struct position_t {
+        long x = 0;
+        long y = 0;
+    };
+
+    using cursor_t = struct {
+        string name;
+        position_t position;
+    };
+
+    WindowManager() = default;
+    WindowManager(Screen screen) : screens(vector<Screen>(screen)) {};
+    WindowManager(vector screens) : screens(screens) {};
+    Screen &moveScreen(Screen &screen, position_t position);
+    Screen &resizeScreen(Screen &screen, dimensions_t dimensions);
+
+private:
+    vector<Screen> screens{ Screen(1920,1080, "primary") };
 };
 
 
