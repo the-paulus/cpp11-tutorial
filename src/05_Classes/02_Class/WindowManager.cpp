3dde83972e1b24228f1225007d9b5253d7960fd6
Paul Lyon <pmlyon@gmail.com>
2018-05-06 02:22:33 -0500

    Defined methods in WindowManager class.

--- src/05_Classes/02_Class/WindowManager.cpp
+++ src/05_Classes/02_Class/WindowManager.cpp
@@ -2,4 +2,121 @@
 // Created by Paulus on 2/17/18.
 //
 
+#include "Screen.hpp"
 #include "WindowManager.hpp"
+
+using std::vector;
+
+WindowManager::WindowManager(Screen* screen) {
+
+    this->screens.push_back(screen);
+
+}
+
+WindowManager::WindowManager(vector<Screen*> screens) {
+
+    this->screens = screens;
+
+}
+
+vector<Screen*> WindowManager::getScreens() {
+
+    return this->screens;
+
+}
+
+Screen* WindowManager::getScreenByID(unsigned long id) {
+
+    return this->screens.at(id);
+
+}
+
+Screen* WindowManager::getScreenByName(string &name) {
+
+    for( unsigned long i = 0; i < this->screens.size(); i++ ) {
+
+        if( this->screens.at(i)->getName().compare(name) == 0 ) {
+
+            return this->screens.at(i);
+        }
+
+
+    }
+
+    return nullptr;
+
+}
+
+vector<Window*> WindowManager::getAllWindows() {
+
+    Screen* screen = nullptr;
+    vector<Window*> windows;
+
+    for(auto screen_it = this->screens.begin(); screen_it != this->screens.end(); screen_it++) {
+
+        screen = *screen_it;
+
+        windows.insert(windows.end(), screen->getWindows().begin(), screen->getWindows().end() );
+
+    }
+
+    return windows;
+
+}
+
+void WindowManager::moveScreen(Screen* screen, position_t position) {
+
+    screen->move(position);
+
+}
+
+void WindowManager::resizeScreen(Screen* screen, dimensions_t dimensions) {
+
+    screen->resize(dimensions);
+
+}
+
+void WindowManager::moveWindow(Window* window, position_t position, Screen* screen = nullptr) {
+
+    Screen* sw;
+
+    if( screen ) {
+
+
+
+    }
+
+    findWindowScreen(window)->move(position);
+
+}
+
+void WindowManager::resizeWindow(Window &window, dimensions_t dimensions) {
+
+}
+
+Screen* WindowManager::findWindowScreen(Window *window) {
+
+    Screen* screen;
+    Window* w;
+
+    for( auto screen_it = this->screens.begin(); screen_it != this->screens.end(); screen_it++ ) {
+
+        screen = *screen_it;
+
+        for( auto window_it = screen->getWindows().begin(); window_it != screen->getWindows().end(); it++ ) {
+
+            w = *window_it;
+
+            if( w == window ) {
+
+                return screen;
+
+            }
+
+        }
+
+    }
+
+    return nullptr;
+
+}
--- src/05_Classes/02_Class/WindowManager.hpp
+++ src/05_Classes/02_Class/WindowManager.hpp
@@ -5,37 +5,53 @@
 #ifndef __WINDOWMANAGER_HPP__
 #define __WINDOWMANAGER_HPP__
 
+#include <iostream>
 #include <vector>
 
-#include "Screen.hpp"
-
+using std::string;
 using std::vector;
 
+class Screen;
+class Window;
+
 class WindowManager {
+
 public:
-    typedef struct dimensions_t {
+
+    typedef struct {
         unsigned long width = 0;
         unsigned long height = 0;
-    };
+    } dimensions_t;
 
-    typedef struct position_t {
+    typedef struct {
         long x = 0;
         long y = 0;
-    };
+    } position_t;
 
-    using cursor_t = struct {
+    typedef struct {
         string name;
         position_t position;
-    };
+    } cursor_t;
 
     WindowManager() = default;
-    WindowManager(Screen screen) : screens(vector<Screen>(screen)) {};
-    WindowManager(vector screens) : screens(screens) {};
-    Screen &moveScreen(Screen &screen, position_t position);
-    Screen &resizeScreen(Screen &screen, dimensions_t dimensions);
+    WindowManager(Screen* screen);
+    WindowManager(vector<Screen*> screens);
+    vector<Screen*> getScreens();
+    Screen* getScreenByID(unsigned long id);
+    Screen* getScreenByName(string &name);
+    vector<Window*> getAllWindows();
+    void moveScreen(Screen *screen, position_t position);
+    void resizeScreen(Screen *screen, dimensions_t dimensions);
+    void moveWindow(Window *window, position_t position);
+    void resizeWindow(Window *window, dimensions_t dimensions);
+    inline unsigned int screenCount() { return screen_count; };
+    inline unsigned int windowCount() { return window_count; };
 
 private:
-    vector<Screen> screens{ Screen(1920,1080, "primary") };
+    Screen* findWindowScreen(Window* window);
+    vector<Screen*> screens;
+    /*mutable unsigned int screen_count = 1;
+    mutable unsigned int window_count = 0;*/
 };
 
 
