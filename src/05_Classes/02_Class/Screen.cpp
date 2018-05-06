78501bb6c8c20369c8f315f73494c6e69e60c300
Paul Lyon <pmlyon@gmail.com>
2018-05-06 01:42:16 -0500

    Updated Screen class.

--- src/05_Classes/02_Class/Screen.cpp
+++ src/05_Classes/02_Class/Screen.cpp
@@ -1,10 +1,19 @@
-#include "screen.hpp"
+#include "Screen.hpp"
+#include "WindowManager.hpp"
 
-Screen::Screen() {
-
+Screen::Screen(string name, unsigned long width, unsigned long height, long x, long y) {
+    this->name = name;
+    this->dimensions.width = width;
+    this->dimensions.height = height;
+    this->position.x = x;
+    this->position.y = y;
 }
 
-Screen::position_t Screen::getCursorPosition() const {
-
+Screen& Screen::move(WindowManager::position_t position) {
+    this->position = position;
+    return *this;
 }
 
+void Screen::render(ostream &os, Window *win) const {
+
+}
--- src/05_Classes/02_Class/Screen.hpp
+++ src/05_Classes/02_Class/Screen.hpp
@@ -1,23 +1,37 @@
 #ifndef __SCREEN_HPP__
 #define __SCREEN_HPP__
 
+#include "Window.hpp"
 #include "WindowManager.hpp"
 
+using std::ostream;
+using std::string;
+using std::vector;
+
 class Screen {
+
 public:
     Screen() = default;
-    Screen(string name, dimensions_t di, position_t pos) : name(name), dimensions(di), position(pos) {};
-    position_t getCursorPosition() const { return cursor.position; } // Implicit inline declaration.
-    inline position_t getCursorPosition() const; // Explicit inline declaration.
-    Screen &move(position_t position);
-    Screen &display(ostream &os) { render(os); return *this; }
-    const Screen &display(ostream &os) const { render(os); return *this; }
+    Screen(string name, WindowManager::dimensions_t di, WindowManager::position_t pos) : name(name), dimensions(di), position(pos) {};
+    Screen(string name, unsigned long width, unsigned long height, long x, long y);
+    string &getName() { return this->name; };
+    WindowManager::position_t getCursorPosition() const { return cursor.position; } // Implicit inline declaration.
+    Screen &move(WindowManager::position_t position);
+    WindowManager::position_t getPosition() { return this->position; };
+    void resize(WindowManager::dimensions_t dimensions) { this->dimensions = dimensions; };
+    WindowManager::dimensions_t getDimensions() { return this->dimensions; };
+    Screen &display(ostream &os, Window *win) { render(os, win); return *this; };
+    const Screen &display(ostream &os, Window *win) const { render(os, win); return *this; };
+    vector<Window*> getWindows() { return windows; };
+
 private:
-    cursor_t cursor;
-    dimensions_t dimensions;
-    position_t position;
+    WindowManager::cursor_t cursor;
+    WindowManager::dimensions_t dimensions;
+    WindowManager::position_t position;
     string name;
-    void render(ostream &os) const { os << contents; }
+    void render(ostream &os, Window *win) const;
+    vector<Window*> windows;
+
 };
 
 #endif
